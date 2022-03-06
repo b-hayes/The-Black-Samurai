a173f70ecee8f99a8024dfd3ccd769495bb43b4a
b-hayes <bradley.hayes@tithe.ly>
2022-03-06 22:13:59 +1100

    Upgraded to unity 2020

--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -58,7 +58,7 @@ namespace UnitySampleAssets._2D
             anim.SetBool("Ground", grounded);
 
             // Set the vertical animation
-            anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+            anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
         }
 
 
@@ -87,7 +87,7 @@ namespace UnitySampleAssets._2D
                 anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
-                rigidbody2D.velocity = new Vector2(move*maxSpeed, rigidbody2D.velocity.y);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !facingRight)
@@ -104,7 +104,7 @@ namespace UnitySampleAssets._2D
                 // Add a vertical force to the player.
                 grounded = false;
                 anim.SetBool("Ground", false);
-                rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
             }
 
 
--- Assets/_SampleAssets/CrossPlatformInput/Scripts/Editor/CrossPlatformInputInitialize.cs
+++ Assets/_SampleAssets/CrossPlatformInput/Scripts/Editor/CrossPlatformInputInitialize.cs
@@ -30,7 +30,7 @@ namespace UnitySampleAssets.CrossPlatformInput.Inspector
             switch (EditorUserBuildSettings.activeBuildTarget)
             {
                 case BuildTarget.Android:
-                case BuildTarget.iPhone:
+                case BuildTarget.iOS:
                 case BuildTarget.WP8Player:
                 case BuildTarget.BlackBerry:
                     EditorUtility.DisplayDialog("Mobile Input",
@@ -62,7 +62,7 @@ namespace UnitySampleAssets.CrossPlatformInput.Inspector
             switch (EditorUserBuildSettings.activeBuildTarget)
             {
                 case BuildTarget.Android:
-                case BuildTarget.iPhone:
+                case BuildTarget.iOS:
                 case BuildTarget.WP8Player:
                 case BuildTarget.BlackBerry:
                     EditorUtility.DisplayDialog("Mobile Input",
@@ -84,19 +84,14 @@ namespace UnitySampleAssets.CrossPlatformInput.Inspector
         private static BuildTargetGroup[] buildTargetGroups = new BuildTargetGroup[]
             {
                 BuildTargetGroup.Standalone,
-                BuildTargetGroup.WebPlayer,
                 BuildTargetGroup.Android,
-                BuildTargetGroup.iPhone,
-                BuildTargetGroup.WP8,
-                BuildTargetGroup.BlackBerry,
+                BuildTargetGroup.iOS,
             };
 
         private static BuildTargetGroup[] mobileBuildTargetGroups = new BuildTargetGroup[]
             {
                 BuildTargetGroup.Android,
-                BuildTargetGroup.iPhone,
-                BuildTargetGroup.WP8,
-                BuildTargetGroup.BlackBerry,
+                BuildTargetGroup.iOS,
             };
 
 
